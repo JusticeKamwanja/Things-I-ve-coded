354258edeaa9d55856d537aa56894471c23fc373
Justice Kamwanja <justicecheshari@gmail.com>
2025-09-19 21:08:46 +0300

    New code
    # Just added some stuff

---
+++ Python/Python Crash Course/Personal Message.py
@@ -0,0 +1,10 @@
+''' Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+would you like to learn some Python today?'''
+
+
+
+name = 'Kamwanja'
+
+message = 'Hi there ' + name + ', how are you doing today?'
+
+print(message)
---
+++ Python/Python Crash Course/name.py
@@ -0,0 +1,6 @@
+'''Assign a messsage to a variable then print the message. Then change the value of the variable into a new message and print the new message.'''
+
+message = 'hello'
+
+message = 'HI THERE YOU'
+print(message)
